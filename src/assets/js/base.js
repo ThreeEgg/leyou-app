6e585301c7989461a2495919e9bab0af5bda56a4
chenxiaolong <chenxiaolong@inter-credit.net>
2020-12-01 19:13:40 +0800

    调整

--- src/assets/js/base.js
+++ src/assets/js/base.js
@@ -1,6 +1,6 @@
 // export const baseUrl = 'http://47.105.159.183:8998';
 // export const baseUrl = 'http://192.168.101.31:8999';
 // export const baseUrl = 'http://127.0.0.1:8998';
-export const baseUrl = 'https://618crazy.cn';
+export const baseUrl = 'https://leyoubuji.com';
 
 
